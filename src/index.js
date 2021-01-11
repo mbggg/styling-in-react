917adc3269b70e504598a93b8bd8f6698848d957
Melissa <34263527+mbggg@users.noreply.github.com>
2021-01-11 15:25:08 -0500

    added images

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom";
 // IMPORTANT: must import the css stylesheet
 import "../public/styles.css";
 
+const img = "https://picsum.photos/200";
+
 ReactDOM.render(
   <div>
     {/* contentEditable allows you to edit element on display page */}
@@ -11,11 +13,31 @@ ReactDOM.render(
     <h1 className="heading" contentEditable="true" spellCheck="false">
       My Favourite Fruits
     </h1>
-    <ul>
-      <li>Cantaloupe</li>
-      <li>Jackfruit</li>
-      <li>Clementines</li>
-    </ul>
+
+    {/* random image */}
+    <img className="food-img" src={img + "?grayscale"} alt="random" />
+
+    {/* cantaloupe pic */}
+    <img
+      className="food-img"
+      src="https://solidstarts.com/wp-content/uploads/Cantaloupe-scaled.jpg"
+      alt="catanloupe"
+    />
+
+    {/* jackfruit pic */}
+    <img
+      className="food-img"
+      src="https://thumbs.dreamstime.com/b/peeled-unpeeled-jackfruit-green-leaves-isolated-white-background-181881616.jpg"
+      alt="jackfruit"
+    />
+
+    {/* clementine pic */}
+    <img
+      className="food-img"
+      src="https://www.oahufresh.com/wp-content/uploads/2017/10/clem-300x300.jpg"
+      alt="clementines"
+    />
   </div>,
+
   document.getElementById("root")
 );
