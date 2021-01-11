85db59cbaba95fe283fc468a0bd7f30de1851937
Melissa <34263527+mbggg@users.noreply.github.com>
2021-01-11 14:02:18 -0500

    switched html a bit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <div>
-    <h1>My Favourite Foods</h1>
+    <h1>My Favourite Fruits</h1>
     <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
+      <li>Cantaloupe</li>
+      <li>Jackfruit</li>
+      <li>Clementines</li>
     </ul>
   </div>,
   document.getElementById("root")
